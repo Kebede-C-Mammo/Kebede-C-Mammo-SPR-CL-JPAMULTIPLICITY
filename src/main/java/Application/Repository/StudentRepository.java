3973fe0e3a9975936ab20e927727fb37641cbe99
Kebede Mammo <mkebede2020@gmail.com>
2024-01-10 15:06:44 +0000

    Done spring jpa multiplicity lab.

--- src/main/java/Application/Repository/StudentRepository.java
+++ src/main/java/Application/Repository/StudentRepository.java
@@ -1,10 +1,15 @@
 package Application.Repository;
 
 import Application.Model.Student;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * JPA Repository interface for the Student entity
  */
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    void save(Optional<Student> student);
 }
